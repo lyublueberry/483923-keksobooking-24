a676ab13f5c93c25726dce2ffc6aa076347846d8
lyublueberry <lara.87@bk.ru>
2021-09-29 00:11:01 +0400

    Функции случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 function getRandomInteger(min, max) {
   if ((min < 0) || (max < 0) || (max <= min)) {
+    /*eslint-disable */
     alert('Вы ввели недопустимое число');
+    /*eslint-enable */
   } else {
     const randInt = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(randInt);
@@ -9,12 +11,14 @@ function getRandomInteger(min, max) {
 
 function getRandomFloating(min, max, numberDecimal) {
   if ((min < 0) || (max < 0) || (max <= min)) {
+    /*eslint-disable */
     alert('Вы ввели недопустимое число');
+    /*eslint-enable */
   } else {
     const randFloat = (min + Math.random() * (max - min));
     return randFloat.toFixed(numberDecimal);
   }
 }
 
-alert(getRandomFloating(5, 18, 4));
-alert(getRandomInteger(-1, 115));
+getRandomFloating(5, 18, 4);
+getRandomInteger(-1, 115);
