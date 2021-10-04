1d9684f05ff097d5a3c1bf7fc93900b85bc68ffd
lyublueberry <lara.87@bk.ru>
2021-10-04 22:07:49 +0400

    Изменения в Больше деталей

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ function getRandomFloating(min, max, numberDecimal) {
 getRandomFloating(5, 18, 4);
 getRandomInteger(1, 115);
 
-/****************************************** */
 
 const NUMBER_GENERATED_OBJECTS = 10;
 const MIN_LAT = 35.65000;
@@ -32,108 +31,66 @@ const author = {
   avatar: 'img/avatars/user', // + numberGenerateForImg() + '.png'
 };
 
-const locationN = {
-  lat: 0.0, //от 35.65000 до 35.70000.
-  lng: 0.0, //от 139.70000 до 139.80000;
-};
-
 const offer = {
-  title: '',
-  address: [locationN.lat, locationN.lng],
-  price: 0, //число — стоимость. Случайное целое положительное число.
   type: ['palace', 'flat', 'house', 'bungalow', 'hotel'], //строка — одно из пяти фиксированных значений
-  rooms: 0, //число — количество комнат. Случайное целое положительное число.
-  guests: 0, // число — количество гостей, которое можно разместить. Случайное целое положительное число.
   checkin: ['12:00', '13:00', '14:00'], //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
   checkout: ['12:00', '13:00', '14:00'], // строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
   features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'], //массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-  description: '', //строка — описание помещения. Придумайте самостоятельно.
   photos: ['https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
     'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg' ],
+    'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],
 };
 
-const getType = () => {
-  let type;
-  for (const key in offer) {
-    if (key === 'type') {
-      return type = (offer[key][getRandomInteger(0, 4)]);
-    }
-  }
-};
+const getType = () => offer.type[getRandomInteger(0, offer['type'].length-1)];
 
-const getPhotos = () => {
-  let photos;
-  for (const key in offer) {
-    if (key === 'photos') {
-      return photos = (offer[key][getRandomInteger(0, 2)]);
-    }
-  }
-};
+const getPhotos = () => offer.photos.slice(0, getRandomInteger(1, offer['photos'].length - 1));
 
-const getFeatures = () => {
-  let features;
-  for (const key in offer) {
-    if (key === 'features') {
-      return features = (offer[key][getRandomInteger(0, 5)]);
-    }
-  }
-};
+const getFeatures = () => offer.features.slice(0, getRandomInteger(1, offer['features'].length-1));
 
-const getCheckout = () => {
-  let checkout;
-  for (const key in offer) {
-    if (key === 'checkout') {
-      return checkout = (offer[key][getRandomInteger(0, 2)]);
-    }
-  }
-};
+const getCheckout = () => offer.checkout[getRandomInteger(0, offer['checkout'].length-1)];
 
-const getCheckin = () => {
-  let checkin;
-  for (const key in offer) {
-    if (key === 'checkin') {
-      return checkin = (offer[key][getRandomInteger(0, 2)]);
-    }
-  }
-};
+const getCheckin = () => offer.checkin[getRandomInteger(0, offer['checkin'].length-1)];
 
 const getNumberGenerateForImg = () => {
-  let numberImg = (getRandomInteger(1, 10)).toString();
-  if (numberImg < 10) {
-    return numberImg = 0 + numberImg;
-  }
-  return numberImg;
+  const numberImg = (getRandomInteger(1, 10));
+  return numberImg < 10 ? 0 + numberImg.toString() : numberImg;
 };
 
-const getNumberGenerateForLat = () => {
-  return locationN.lat = (getRandomFloating(MIN_LAT, MAX_LAT, NUMBER_DECIMAL));
-};
+const getNumberGenerateForLat = () =>
+  (getRandomFloating(MIN_LAT, MAX_LAT, NUMBER_DECIMAL));
 
-const getNumberGenerateForLng = () => {
-  return locationN.lng = (getRandomFloating(MIN_LNG, MAX_LNG, NUMBER_DECIMAL));
-};
+const getNumberGenerateForLng = () =>
+  (getRandomFloating(MIN_LNG, MAX_LNG, NUMBER_DECIMAL));
 
-const createAdObject = () => {
-  return {
-    address: getNumberGenerateForLat() + ', ' + getNumberGenerateForLng(),
-    title: 'Уютная квартирка',
-    avatar: author.avatar + getNumberGenerateForImg() + '.png',
-    price: getRandomInteger(800, 5000),
-    rooms: getRandomInteger(1, 5),
-    guests: getRandomInteger(1, 10),
-    checkin: getCheckin(),
-    checkout: getCheckout(),
-    description: 'Замечательное жилье',
-    type: getType(),
-    photos: getPhotos(),
-    features: getFeatures(),
+const createAdObject = (value, index) => {
+  const location = {
+    lat: getNumberGenerateForLat(),
+    lng: getNumberGenerateForLng(),
   };
 
+  return {
+    author: {
+      avatar: `${author.avatar}${getNumberGenerateForImg()}.png`,
+    },
+    offer: {
+      address: `${location.lat} , ${location.lng}`,
+      title: `Уютная квартирка ${index + 1}`,
+      price: getRandomInteger(800, 5000),
+      rooms: getRandomInteger(1, 5),
+      guests: getRandomInteger(1, 10),
+      checkin: getCheckin(),
+      checkout: getCheckout(),
+      description: `Замечательное жилье ${index + 1}`,
+      type: getType(),
+      photos: getPhotos(),
+      features: getFeatures(),
+    },
+    location: location,
+  };
 };
 
-
 const similarAdObjects = Array.from({
-  length: NUMBER_GENERATED_OBJECTS}, createAdObject);
+  length: NUMBER_GENERATED_OBJECTS }, createAdObject);
 
 console.log(similarAdObjects);
+
