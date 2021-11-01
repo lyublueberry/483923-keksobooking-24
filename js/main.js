e51871e458e391304b92e2470e675631141c81fe
lyublueberry <lara.87@bk.ru>
2021-11-01 23:36:31 +0400

    исправление ошибки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import {createCards} from './generating-template-element.js';
 import {setInactiveState} from './state-active-inactive.js';
 import {checkFieldIsValidTitle} from './working-with-forms.js';
 import {checkFieldIsValidPrice} from './working-with-forms.js';
@@ -6,15 +5,11 @@ import {onChangeCapacityAndRoomNumber} from './working-with-forms.js';
 import {onChangeTypeHousingAndPriceNight} from './working-with-forms.js';
 import {onChangeTimeIn} from './working-with-forms.js';
 import {onChangeTimeOut} from './working-with-forms.js';
-//import {serverWork} from './work-with-server.js';
 import {addCardsInMarker} from './work-with-map.js';
-//import {publishAd} from './working-with-forms.js';
-
 import {setUserFormSubmit, clickResetForm} from './working-with-forms.js';
 
 
 addCardsInMarker();
-createCards();
 setInactiveState();
 checkFieldIsValidTitle();
 checkFieldIsValidPrice();
